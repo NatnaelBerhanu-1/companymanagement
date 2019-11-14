7781472d666ed5c41db9b2ac7f383711521bfc56
unknown <bereketheramo25@gmail.com>
2019-11-14 20:12:46 +0300

    event completed

--- src/components/bankEvent.vue
+++ src/components/bankEvent.vue
@@ -2,32 +2,35 @@
 <div class="container-fluid">
       <Header v-bind:add_link="'/add/company'"/>
       <div class="container bg-white p-3 mb-5 shadow bank-event">
-          <h3 class="pt-3">Bank Event</h3>
+          <h5 class="pt-3">Bank Event</h5>
          <table class="table table-striped table-bordered table-responsive-xl">
-              <thead>
-                  <tr>
-                      <th scope="col">Bank</th>
-                      <th scope="col">Title</th>
-                      <th scope="col">Date</th>
-                      <th scope="col">Time</th>
-                     
-                  </tr>
-
-              </thead>
+              
               <tbody>
                   <tr>
-                      <td>Berhan</td>
-                      <td>Meeting</td>
-                      <td>11/12/2019</td>
-                      <td>9:00am</td>
-                     
+                      <th rowspan="2" class="pt-5 pl-4">Berhan Bank</th>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td> 
                   </tr>
                   <tr>
-                      <td>Dashen</td>
-                      <td>Training</td>
-                      <td>17/12/2019</td>
-                      <td>9:00am</td>
-                     
+                      <td>2:00am<br/>Customer Support meeting</td>
+                      <td>4:00am<br/>Shareholders meeting</td>
+                      <td>6:00am<br/>training</td>
+                      <td>10:00am<br/>general meeting</td>
+                  </tr>
+                  <tr>
+                      <th rowspan="2" class="pt-5 pl-4">CBE</th>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td>
+                      <td>12/04/2021</td> 
+                  </tr>
+                  <tr>
+                      <td>2:00am<br/>general meeting</td>
+                      <td>4:00am<br/>Share holders meeting</td>
+                      <td>6:00am<br/> accountants training</td>
+                      <td>10:00am<br/>Meeting</td>
                   </tr>
               </tbody>
           </table>
